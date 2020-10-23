3b77f2776f2539ba1a6e65f209f82c8edb1bfc43
alexpsimone <alex.p.simone@gmail.com>
2020-10-23 19:52:04 +0000

    added Homepage rendered content

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,9 +1,14 @@
 "use strict";
 
+// this is the Homepage.render function
 function Homepage() {
   return (
     <div>
-      This component needs to be implemented.
+      <p>Welcome to Balloonicorn's Trading Card game!</p>
+      <br />
+      <img src="/static/img/balloonicorn.jpg" />
+      <br />
+      <a href="/cards">Click here to view all trading cards!</a>
     </div>
   );
 }
